4811688940961072035c434fac476b35824acaed
JiLi <150696404+JiLisx@users.noreply.github.com>
2024-09-21 22:40:11 +0800

    增加多个需要遍历的根目录列表

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 def read_patents(file_path):
     with open(file_path, 'r') as file:
         patents = set(line.strip() for line in file.readlines())
-    print(f"Total patents read: {len(patents)}")
+    print(f"Total granted patents read: {len(patents)}")
     return patents
 
 def list_downloaded_pdfs(root_path):
@@ -12,9 +12,10 @@ def list_downloaded_pdfs(root_path):
     total_files = 0
 
     # 计算 PDF 文件总数
-    for dirpath, _, filenames in os.walk(root_path):
-        total_files += len([f for f in filenames if f.endswith('.pdf')])
-    print(f"Total PDF files found: {total_files}")
+    for root_path in root_paths:
+        for dirpath, _, filenames in os.walk(root_path):
+            total_files += len([f for f in filenames if f.endswith('.pdf')])
+    print(f"Total patent PDF files found: {total_files}")
 
     with tqdm(total=total_files, desc="Scanning PDF files") as pbar:
         for dirpath, _, filenames in os.walk(root_path):
@@ -24,12 +25,12 @@ def list_downloaded_pdfs(root_path):
                     clean_filename = filename.replace('*', '').replace('.pdf', '').replace('-p920mint', '')
                     downloaded_pdfs.add(clean_filename)
                 pbar.update(1)
-    print(f"Total PDF files processed: {len(downloaded_pdfs)}")
+    print(f"Total PDF files processed : {len(downloaded_pdfs)}")
     return downloaded_pdfs
 
-def find_extra_pdfs(grant_pats, downloaded_pdfs):
+def find_missing_pdfs(grant_pats, downloaded_pdfs):
     extra_pdfs = grant_pats - downloaded_pdfs
-    print(f"Total extra PDFs: {len(extra_pdfs)}")
+    print(f"Total missing PDFs: {len(find_missing_pdfs)}")
     return extra_pdfs
 
 def write_missing_pdfs(missing_pdfs, file_path):
@@ -39,9 +40,13 @@ def write_missing_pdfs(missing_pdfs, file_path):
     print(f"Extra PDFs written to: {file_path}")
 
 if __name__ == "__main__":
-    root_path = "/data/home/jdang/SIPO_PDF_B"
-    grant_file = os.path.join(root_path, "grant_pnr_all.txt")
-    extra_list_file = os.path.join(root_path, "missing_pdfs.txt")
+     # 定义需要遍历的根目录列表
+    root_paths = [
+        "/data/home/jdang/SIPO_PDF_B",
+        "/data/home/liji/dload/dload2023"
+    ]
+    grant_file = os.path.join(root_path, "grant_pnr_all2406.txt")
+    extra_list_file = os.path.join(root_path, "missing_pdfs2406.txt")
 
     grant_pats = read_patents(grant_file)
     downloaded_pdfs = list_downloaded_pdfs(root_path)
@@ -51,7 +56,7 @@ if __name__ == "__main__":
     write_missing_pdfs(extra_pdfs, extra_list_file)
 
     if not extra_pdfs:
-        print("No extra PDFs found.")
+        print("No missing PDFs found.")
     else:
         print(f"Missing PDFs list has been written to {extra_list_file}")
 
