b95f625aceec5365d8f3a60472be0714cfc599cb
Dongbo Shi <shidongbo@sjtu.edu.cn>
2025-01-12 23:36:18 +0800

    modify second-page codes

--- second_pg.py
+++ second_pg.py
@@ -7,31 +7,37 @@ import glob
 
 ipt_dir = "/data/home/jdang/SIPO_PDF_B/ocr_results"
 opt_dir = "/data/home/jdang/SIPO_PDF_B/ocr_results"
+optfile = "second_pg.txt"
+
 if not os.path.exists(opt_dir):
     os.mkdir(opt_dir)
 
+
 def loop_files():
-    for file_name in glob.glob(os.path.join(ipt_dir, '*','*.json')):
+    for file_name in glob.glob(os.path.join(ipt_dir, '*', '*.json')):
         print(file_name)
-        with open(file_name,"r") as f:
+        with open(file_name, "r") as f:
             for line in f.readlines():
                 descs = json.loads(line)
-                if descs.__class__ == list :
+                if descs.__class__ == list:
                     for desc in descs:
-                        pnr = re.findall("CN[0-9]+[A-Z]",desc["pnr"])[0]
+                        pnr = re.findall("CN[0-9]+[A-Z]", desc["pnr"])[0]
                         left = desc["left"].replace(" ", "")
                         right = desc["right"].replace(" ", "")
-                        if re.search("续页", left, re.IGNORECASE) is not None or re.search("续页", right, re.IGNORECASE) is not None:
-                            with open(opt_dir + "/second_pg.txt", "a") as g:
+                        if re.search("续页", left, re.IGNORECASE) is not None or re.search("续页", right,
+                                                                                           re.IGNORECASE) is not None:
+                            with open(os.path.join(opt_dir, optfile), "a") as g:
                                 g.write(pnr + "\n")
                 else:
                     desc = descs
-                    pnr = re.findall("CN[0-9]+[A-Z]",desc["pnr"])[0]
+                    pnr = re.findall("CN[0-9]+[A-Z]", desc["pnr"])[0]
                     left = desc["left"].replace(" ", "")
                     right = desc["right"].replace(" ", "")
-                    if re.search("续页", left, re.IGNORECASE) is not None or re.search("续页", right, re.IGNORECASE) is not None:
-                        with open(opt_dir + "/second_pg.txt", "a") as g:
+                    if re.search("续页", left, re.IGNORECASE) is not None or re.search("续页", right,
+                                                                                       re.IGNORECASE) is not None:
+                        with open(os.path.join(opt_dir, optfile), "a") as g:
                             g.write(pnr + "\n")
+
 
 if __name__ == '__main__':
     loop_files()
